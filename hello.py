bf08066b4622db13974d9f7b5180ad06666f086f
RamonCamposC <106851749+RamonCamposC@users.noreply.github.com>
2022-06-13 18:01:07 -0500

    Add files via upload
    Here we have a hello word in python

---
+++ hello.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 28 12:53:38 2021
+
+@author: jeff4
+"""
+
+def hello():
+    """Print "Hello World" and return None."""
+    print("Later World")
+    
+hello()
